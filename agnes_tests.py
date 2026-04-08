9354635fb7edc6e73844e6ac829e6d9a2e2c9342
Agnese Bumbiere <213814892+AgneseBu@users.noreply.github.com>
2026-04-08 14:46:53 +0200

    adding straucture for displaying the maze

--- agnes_tests.py
+++ agnes_tests.py
@@ -1,11 +1,20 @@
 from file_reader import read_maze_file
 from file_reader import decode_cell
 from file_reader import process_directions
+from display import create_grid
 
-# Place to test my functions
+# a place to test my functions
+
+# test file_reader
 
 directions = ['S', 'W', 'S', 'E', 'S', 'W', 'S', 'E', 'S', 'W', 'S', 'S', 'S', 'E', 'E', 'S', 'E', 'E', 'E', 'E', 'N', 'E', 'E', 'S', 'E', 'S', 'E', 'E', 'S', 'S', 'S', 'E', 'E', 'E', 'S', 'S', 'S', 'E', 'E', 'E', 'N', 'N', 'E', 'N', 'E', 'E']
 
 print(read_maze_file("temp_mock_maze.txt"))
 print(decode_cell('A'))
 print(process_directions(directions, (1, 1)))
+
+# test display
+
+result = read_maze_file("temp_mock_maze.txt")
+maze = result[0]
+create_grid(maze, 25, 20)
--- display.py
+++ display.py
@@ -1,2 +1,20 @@
 # takes the 2D grid of cells and draws them visually
 # in the terminal using ASCII characters
+
+def create_grid(maze: list, width: int, height: int) -> None:
+    """
+    Draw the grid with our given maze data using block symbol
+    """
+    # we are skipping bottom line as that is the following rows top line
+    # for each row in maze
+        # draw the top line 
+            # for each cell in a row:
+                # check if north wall exists & draw █████ or spaces if no wall
+        # draw the midle line:
+            # for each cell in row:
+                # check if wall exists & draw █
+                # draw 3 spcaes (the middle bit)
+                # check if east wall exists & draw █
+    # after all the ows:
+        # draw the final bottom line for the last row
+    pass
