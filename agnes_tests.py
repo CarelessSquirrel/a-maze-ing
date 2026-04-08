27b041b8dee368cc2ca6eb313882b212bc243806
Agnese Bumbiere <213814892+AgneseBu@users.noreply.github.com>
2026-04-08 13:11:52 +0200

    adding cell decoding and path processing

--- agnes_tests.py
+++ agnes_tests.py
@@ -1,5 +1,11 @@
 from file_reader import read_maze_file
+from file_reader import decode_cell
+from file_reader import process_directions
 
 # Place to test my functions
 
+directions = ['S', 'W', 'S', 'E', 'S', 'W', 'S', 'E', 'S', 'W', 'S', 'S', 'S', 'E', 'E', 'S', 'E', 'E', 'E', 'E', 'N', 'E', 'E', 'S', 'E', 'S', 'E', 'E', 'S', 'S', 'S', 'E', 'E', 'E', 'S', 'S', 'S', 'E', 'E', 'E', 'N', 'N', 'E', 'N', 'E', 'E']
+
 print(read_maze_file("temp_mock_maze.txt"))
+print(decode_cell('A'))
+print(process_directions(directions, (1, 1)))
--- file_reader.py
+++ file_reader.py
@@ -39,28 +39,53 @@ def read_maze_file(filename: str) -> tuple:
     return (maze, entry, exit, path)
 
 
-def decode_cell(maze: str) -> dict:
+# A cell is one "pixel" of the maze that represents where a wall will be
+def decode_cell(cell: str) -> dict:
     """
     Take the 2D grid created in read_maze_file() and turn 
     the hex charcters into walls
     """
     # take the first character from the first maze line
     # convert it from a hex to a regular int
-    # check each of the 4 bits of this int:
+    value = int(cell, 16)
+    # check each of the 4 bits of this int with a mask:
     # LSB - least significant bit
         # bit 0 (LSB)= north wall
         # bit 1 = east wall
         # bit 2 = south wall
         # bit 3 = west wall
+    north = bool(value & 1)   # checks bit 0
+    east = bool(value & 2)    # checks bit 1
+    south = bool(value & 4)   # checks bit 2
+    west = bool(value & 8)    # checks bit 3
     # return a dict something like:
     # {"north": True, "east": False, "south": True, "west": False}
-    pass
+    cell_walls = {
+        "north": north,
+        "east": east,
+        "south": south,
+        "west": west
+        }
+    return (cell_walls)
 
 
-def process_directions(path: str) -> list:
+def process_directions(path: list, entry: tuple) -> list:
     """
     take the direction string and put coordinates from it
     into a list, they will represent the solution path
     """
-    pass
-
+    current_pos = entry
+    coordinates = []
+    print(path)
+    for direction in path:
+        x, y = current_pos
+        if direction == 'S':
+            current_pos = (x, y + 1)
+        if direction == 'N':
+            current_pos = (x, y - 1)
+        if direction == 'E':
+            current_pos = (x + 1, y)
+        if direction == 'W':
+            current_pos = (x - 1, y)
+        coordinates.append(current_pos)
+    return (coordinates)
